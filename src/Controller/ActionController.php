dc8772eb2b57eb5c56fa8554ea7848b5992e383a
BaptisteHouques <baptistehouques38@gmail.com>
2023-02-02 18:47:27 +0100

    composant action

--- src/Controller/ActionController.php
+++ src/Controller/ActionController.php
@@ -26,8 +26,6 @@ class ActionController extends AbstractController
         foreach ($actions as $action) {
             $userAction = $userActionRepository->findOneBy(['action' => $action, 'user' => $security->getUser()]);
             $action->status = $userAction ? $userAction->getStatus() : 3;
-            $nbInscrit = count($userActionRepository->findBy(['action' => $action, 'status' => 1]));
-            $action->nbInscrit = $nbInscrit;
         }
 
         return $this->render('action/index.html.twig', [
@@ -96,6 +94,8 @@ class ActionController extends AbstractController
         }
 
         $is_responsible = $action->getResponsible() === $connectedUser ? 1 : 0;
+        $nbInscrit = count($userActionRepository->findBy(['action' => $action, 'status' => 1]));
+        $action->nbInscrit = $nbInscrit;
 
         return $this->render('action/show.html.twig', [
             'action' => $action,
