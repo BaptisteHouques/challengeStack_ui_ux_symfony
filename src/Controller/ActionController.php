cb05304107308138b28d2652def4df7676a0b451
Lucas <lucas.humbert.28@gmail.com>
2023-02-02 08:47:14 +0100

    composant affichage evenement

--- src/Controller/ActionController.php
+++ src/Controller/ActionController.php
@@ -34,7 +34,7 @@ class ActionController extends AbstractController
 //            }
             $actionRepository->save($action, true);
 
-            return $this->redirectToRoute('app_action_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_action_show', ['id' => $action->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('action/new.html.twig', [
@@ -60,7 +60,7 @@ class ActionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $actionRepository->save($action, true);
 
-            return $this->redirectToRoute('app_action_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_action_show', ['id' => $action->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('action/edit.html.twig', [
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ActionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ActionRepository $actionRepository): Response
     {
+        $qb = $actionRepository->createQueryBuilder('a')
+        ->where('a.date > CURRENT_DATE()')
+        ->orderBy('a.date', 'ASC')
+        ->setMaxResults(3)
+        ->getQuery();
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'events' => $qb->execute(),
         ]);
     }
 
